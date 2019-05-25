25bd59adbdc084354c1443d22ef33f4dacb0a200
jansseneda-adobe <danimaejanssen@gmail.com>
2019-05-25 01:50:05 -0500

    better logging, better life

--- App.py
+++ App.py
@@ -1,22 +1,23 @@
-import customlogging
 import logging
-import click
-import time
 import re
-from subprocess import Popen, PIPE, STDOUT, check_output, call, CalledProcessError
+import time
+import click
+import customlogging
+from subprocess import check_output, CalledProcessError
 
 customlogging.initLogger()
 logger = logging.getLogger("vbox")
 
 
 @click.group()
-@click.option('--debug/--no-debug', default=False)
+@click.option('--debug', is_flag=True)
 @click.option('-l', '--vmlist',
               help="comma separated list of VMs (default is 'all')",
               type=str,
               nargs=1)
 @click.pass_context
 def cli(ctx, **kwargs):
+    logger.setLevel(logging.DEBUG if kwargs['debug'] else logging.INFO)
     vmlist = kwargs['vmlist']
     if vmlist is not None:
         vmlist = [v.strip().replace("\"", "") for v in vmlist.split(",")]
@@ -35,13 +36,20 @@ def cli(ctx, **kwargs):
               nargs=1,
               default=10)
 def start(ctx, **kwargs):
+    logger.info("Starting all virtualmachines... ")
+    logger.info("Parameters: ")
+    logger.info(kwargs)
+
     start_delay = kwargs['start_delay']
     vmlist = ctx.obj['vmlist']
 
+    logger.info("Removing already running VM's from working list... ")
     for v in parse_vm_list(shell_exec("vboxmanage list runningvms")[0]):
         if v in vmlist:
             logger.info("Skipping " + v + ", already running!")
             vmlist.remove(v)
+
+    logger.info("Final VM list: " + str(vmlist))
 
     for vm in vmlist:
         success = start_single_vm(vm)
@@ -61,13 +69,21 @@ def start(ctx, **kwargs):
               nargs=1,
               default='acpi')
 def stop(ctx, **kwargs):
+    logger.info("Stopping all virtualmachines... ")
+    logger.info("Parameters: ")
+    logger.info(kwargs)
+
     method = "acpipowerbutton" if kwargs['method'] == 'acpi' else "poweroff"
     vmlist = ctx.obj['vmlist']
+
+    logger.info("Removing already stopped VM's from working list... ")
     running = get_running_vms()
     for v in vmlist:
         if v not in running:
             logger.info("Skipping " + v + ", already stopped!")
             vmlist.remove(v)
+
+    logger.info("Final VM list: " + str(vmlist))
 
     if len(vmlist) == 0:
         logger.info("No VM's to stop... exiting!")
@@ -76,18 +92,24 @@ def stop(ctx, **kwargs):
     for vm in vmlist:
         stop_single_vm(vm, method)
 
+    logger.info("Finished shutdown sequence for all VM's!")
+
+
 def get_running_vms():
+    logger.debug("Fetch running VM's... ")
     running = parse_vm_list(shell_exec("vboxmanage list runningvms")[0])
     return running if running else []
 
 
 def start_single_vm(name):
+    logger.info("Attempting to start vm: " + name)
     r, c = shell_exec("vboxmanage startvm \"" + name + "\" --type headless")
     if not c: logger.warning("Failed to boot vm: " + name)
     return c
 
 
 def stop_single_vm(name, method):
+    logger.info("Attempting to stop vm: " + name)
     r, c = shell_exec("vboxmanage controlvm \"" + name + "\" " + method + " --type headless")
     if not c:
         logger.warning("Failed to stop vm: " + name)
